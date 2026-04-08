6448a668146aed86b5aa1d5f8b39301479bfb95c
Tamimkhan7 <mdtamimkhan842@gmail.com>
2026-04-08 10:02:05 +0600

    updated authentication system

---
+++ UniversityClubAPI/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniversityClubAPI.Data;
+using UniversityClubAPI.DTOs;
+using UniversityClubAPI.Models;
+
+namespace UniversityClubAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _config;
+
+        public AuthController(AppDbContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDTO dto)
+        {
+            var user = new User
+            {
+                Name = dto.Name,
+                Email = dto.Email,
+                Password = dto.Password
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginDTO dto)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Email == dto.Email && x.Password == dto.Password);
+            if (user == null) return Unauthorized();
+
+            var token = JwtHelper.GenerateToken(user.Email, _config);
+            return Ok(token);
+
+        }
+    }
+}
+
+
